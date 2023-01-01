05de05b2a2dfcb653893a1276c150bbfc0629e95
Taras Papka <taras.papka@protonmail.com>
2023-01-02 00:05:18 +0200

    created ducks, move of duck and place of dispatch

--- script.js
+++ script.js
@@ -91,19 +91,125 @@
 
 /*
 створити 3 качки
-зробити випадковий руз качкам
+зробити випадковий рух качкам
+   - виліт з випадкового місця
+   - випадковий напрям
+   - 6 варіантів руху качки: ліво, право, ліво вних, ліво вверх, право вниз, право вверх
 коли качка долітає до границі, змінити напрям
+зробити випадковий колір качки
+
 
 */
+
+const score = 10;
+
 //знаходимо контейнер для вставки качок
 const gameArea = document.querySelector('.game-area');
 
 //створення качки
-function createDuck(top, left, type) {
-    let ducks = `<div class="duck ${type}-duck-left" style="left: ${left}; top: ${top}"></div>`;
-    gameArea.innerHTML += ducks;
+// function createDuck(top, left, type) {
+//     let ducks = `<div class="duck ${type}-duck-left" style="left: ${left}; top: ${top}"></div>`;
+//     gameArea.innerHTML += ducks;
+// }
+
+// інштй спосіб створення функції 
+
+function createDuck(left, type) {
+    let duck = document.createElement('div');
+    duck.className = `duck ${type}-duck-left`;
+    duck.style.top ="100%";
+    duck.style.left = left;
+    gameArea.appendChild(duck);
+
+    moveDuck(duck, type);
 }
 
-// викликаємо функцію та пережаємо параметри
-createDuck('10%', '40%', 'red');
-createDuck('50%', '10%', 'black');
+// викликаємо функцію та передаємо параметри
+createDuck(getRandomInt(0, 100) + '%', 'red');
+createDuck(getRandomInt(0, 100) + '%', 'black');
+createDuck(getRandomInt(0, 100) + '%', 'red');
+createDuck(getRandomInt(0, 100) + '%', 'black');
+
+// функція руху качок 
+
+function moveDuck(duck, type) {
+
+    let imageDuck = 0;
+
+    const timer = setInterval(function () {
+
+       
+
+        //     //змінюємо картинки
+        imageDuck += 1;
+
+        //     //умова якщо немає більше картинки
+        if (imageDuck > 2) {
+            imageDuck = 0;
+                }
+
+        moveTopRight(duck, type, imageDuck);
+        
+            //     //рух качки
+        // duck.style.left = duck.offsetLeft - 30 + 'px';
+        // duck.style.top = duck.offsetTop - 30 + 'px';
+
+            //     //обмеження, щоб качка не вийшла за екран
+            if (duck.offsetLeft < 0) {
+                clearInterval(timer);
+            }
+        }, 200);
+}
+
+// 6 варіантів руху качок 
+
+// Кожна функція буде робити зміну картинки 
+// Функція змінює координати 
+
+function moveRight(duck, type, imageDuck) {
+  //шукаємо шлях до картинки
+    duck.style.backgroundImage = `url(assets/images/duck/${type}/right/${imageDuck}.png)`;
+    // задаємо напрям руху 
+    duck.style.left = duck.offsetLeft + score + 'px';
+}
+
+function moveLeft(duck, type, imageDuck) {
+    duck.style.backgroundImage = `url(assets/images/duck/${type}/left/${imageDuck}.png)`;
+    duck.style.left = duck.offsetLeft - score + 'px';
+}
+
+function moveTopRight(duck, type, imageDuck) {
+    duck.style.backgroundImage = `url(assets/images/duck/${type}/top-right/${imageDuck}.png)`;
+    duck.style.left = duck.offsetLeft + score + 'px';
+    duck.style.top = duck.offsetTop - score + 'px';
+}
+
+function moveTopLeft(duck, type, imageDuck) {
+    duck.style.backgroundImage = `url(assets/images/duck/${type}/top-left/${imageDuck}.png)`;
+    duck.style.left = duck.offsetLeft - score + 'px';
+    duck.style.top = duck.offsetTop - score + 'px';
+}
+
+function moveDownRight(duck, type, imageDuck) {
+    duck.style.backgroundImage = `url(assets/images/duck/${type}/top-right/${imageDuck}.png)`;
+    duck.style.left = duck.offsetLeft + score + 'px';
+    duck.style.top = duck.offsetTop + score + 'px';
+}
+
+function moveDownLeft(duck, type, imageDuck) {
+    duck.style.backgroundImage = `url(assets/images/duck/${type}/top-left/${imageDuck}.png)`;
+    duck.style.left = duck.offsetLeft - score + 'px';
+    duck.style.top = duck.offsetTop + score + 'px';
+}
+
+
+
+
+
+// Рандомний вибір місця вильоту качки 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включається, мінімум включається
+}
+
